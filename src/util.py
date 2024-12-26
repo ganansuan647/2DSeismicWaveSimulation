c5c4d69cfdd2b4aef08e7d992dd977c5032fd10f
Gou Lingyun <62201655+ganansuan647@users.noreply.github.com>
2024-12-26 15:15:15 +0800

    交错网格计算函数

--- src/util.py
+++ src/util.py
@@ -163,3 +163,49 @@ def update_step(p: np.ndarray,
         p_new = 2.0 * p - p_old + (v ** 2) * (dt ** 2) * lap
 
     return p_new, p
+
+@njit(parallel=True, fastmath=True)
+def update_acoustic_staggered(
+    vx, vz, P,
+    rho_vx, rho_vz, rho_p,   # 可能在各自网格上定义好的密度值
+    K_p,                     # 压力点处的体积模量
+    dx, dz, dt,
+    coeff_x, coeff_z        # 分别用于 x 向、z 向的一阶差分系数
+):
+    """
+    单步更新 2D 交错网格下的速度 (vx, vz) 和压力 P。
+    这里的 rho_vx, rho_vz, rho_p, K_p 等都可以是二维数组, 显式包含介质变化。
+    压力场P放在整点上，速度场vx, vz放在半整点上。
+            j-1        j         j+1
+    i+0.5   *----vz----*----vz----*
+            |          |          |
+    i      vx        (P)        vx
+            |          |          |
+    i-0.5   *----vz----*----vz----*
+    """
+    # --- 1) 更新 v_x (对 P 做 x 向差分) ---
+    # P 在 (i,j)， v_x 在 (i,j+0.5)，所以要对 P 做插值或直接采用交错索引。
+    # 这里为简化，假设数组已做好对齐(仅演示原理)，实际需注意下标偏移。
+    dP_dx = fd_derivative(P, coeff_x, dx, axis=1)
+    # vx^new = vx^old - dt * (1/rho_vx) * dP/dx
+    for iz in prange(vx.shape[0]):
+        for ix in prange(vx.shape[1]):
+            vx[iz, ix] = vx[iz, ix] - dt * (1.0 / rho_vx[iz, ix]) * dP_dx[iz, ix]
+
+    # --- 2) 更新 v_z (对 P 做 z 向差分) ---
+    dP_dz = fd_derivative(P, coeff_z, dz, axis=0)
+    for iz in prange(vz.shape[0]):
+        for ix in prange(vz.shape[1]):
+            vz[iz, ix] = vz[iz, ix] - dt * (1.0 / rho_vz[iz, ix]) * dP_dz[iz, ix]
+
+    # --- 3) 更新 P (对 v_x, v_z 求散度) ---
+    # P 在 (i,j)， v_x 在 (i,j+0.5)， v_z 在 (i+0.5,j)
+    # 因此散度 approx d(vx)/dx + d(vz)/dz
+    dvx_dx = fd_derivative(vx, coeff_x, dx, axis=1)
+    dvz_dz = fd_derivative(vz, coeff_z, dz, axis=0)
+    for iz in prange(P.shape[0]):
+        for ix in prange(P.shape[1]):
+            div_v = dvx_dx[iz, ix] + dvz_dz[iz, ix]
+            P[iz, ix] = P[iz, ix] - dt * K_p[iz, ix] * div_v
+
+    return vx, vz, P
